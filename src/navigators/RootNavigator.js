a23e400da77f37c6908314dcaf62bd40fe3be8f0
Damodar Lohani <dlohani48@gmail.com>
2017-11-14 09:02:13 +0545

    adding header style to root navigator navigation options

--- src/navigators/RootNavigator.js
+++ src/navigators/RootNavigator.js
@@ -1,9 +1,14 @@
 import { StackNavigator } from 'react-navigation';
 import HomeScreen  from '../screens/home';
+import globalStyles from '../styles/GlobalStyles';
 
 export default RootNavigator = StackNavigator({
     Home: { screen: HomeScreen },
 },{
+    navigationOptions: ({navigation})=>({
+        headerStyle: globalStyles.headerStyle,
+        headerTitleStyle: globalStyles.headerTitleStyle
+    }),
     cardStyle: {
         backgroundColor: '#fff',
     }
