d7d888ff27de209bfeb38ec59fa7892c88b01df6
Damodar Lohani <dlohani48@gmail.com>
2017-11-14 12:44:25 +0545

    android status bar color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StatusBar } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import createStore from './store';
 import RootNav from './navigators/RootNavigator';
 import bootstrap from './bootstrap';
+import globalStyles from './styles/GlobalStyles';
 
 const { persistor, store } = createStore();
 
@@ -23,6 +24,10 @@ let App = () => {
         loading={<Text>Loading..</Text>}
         onBeforeLift={onBeforeLift}
         persistor={persistor}>
+        <StatusBar
+          backgroundColor={globalStyles.$statusBarColor}
+          barStyle="light-content"
+          />
         <RootNav />
       </PersistGate>
     </Provider>
