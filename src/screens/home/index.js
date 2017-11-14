c5940eeb853cc3f73c1c93a19afadf94c7a32e17
Damodar Lohani <dlohani48@gmail.com>
2017-11-14 13:31:53 +0545

    simple redux implementation

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,22 +1,46 @@
 import React from 'react';
 import { View } from 'react-native';
+import { connect } from 'react-redux';
 import Text from '../../components/text/Text';
 import styles from './styles';
+import Button from '../../components/button/Button';
+import { updateLuckyNumber } from './actions';
 
 class HomeScreen extends React.Component{
     static navigationOptions = ({navigation}) => ({
         title: 'Home'
     })
+    state = {
+        loading: false
+    }
+
+    generateLuckyNumber = () => {
+        this.setState({loading: true});
+        let newnum = Math.floor(Math.random() * 10);
+        this.props.dispatch(updateLuckyNumber(newnum));
+        this.setState({loading: false});
+    }
+
     render(){
+        const { homeState: { luckyNumber } } = this.props;
         return(
             <View style={styles.container}>
                 <Text h1> Heading 1 </Text>
                 <Text h2> Heading 2 </Text>
                 <Text h3> Heading 3 </Text>
                 <Text h4> Heading 4 </Text>
+
+                <Text style={{marginTop: 20}}>Simple logic showing implementation of Redux, and redux persist. View code to learn more.</Text>
+                <Text style={{marginTop: 20}} h4>Lucky number</Text>
+                <Text h1>{luckyNumber}</Text>
+                <Button loading={this.state.loading} onPress={this.generateLuckyNumber} accent title="Test My Luck" />
             </View>
         )
     }
 }
 
-export default HomeScreen;
+const mapStateToProps = (state) => ({
+      homeState: state.homeState
+});
+
+export default connect(mapStateToProps)(HomeScreen);
--- src/screens/home/reducer.js
+++ src/screens/home/reducer.js
@@ -1,9 +1,15 @@
 const initialState = {
-
+  luckyNumber: 7
 };
   
 export default (state = initialState, action) => {
   switch (action.type) {
+    case 'UPDATE_LUCKY_NUMBER': {
+      return {
+        ...state,
+        luckyNumber: action.luckyNumber
+      }
+    }
     default: {
       return state;
     }
