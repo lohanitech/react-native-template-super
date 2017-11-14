307df4f1bae48fe3e9eca6f057deca695d5eb65d
Damodar Lohani <dlohani48@gmail.com>
2017-11-14 09:02:31 +0545

    global styles object to keep common styles

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,10 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default globalStyles = EStyleSheet.create({
+    headerStyle: {
+        backgroundColor: '$primaryColor',
+    },
+    headerTitleStyle: {
+        color: '$textOnPrimaryColor'
+    }
+})
