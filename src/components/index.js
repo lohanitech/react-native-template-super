88178c8c5741884237c869cce07c81388af1da99
Damodar Lohani <dlohani48@gmail.com>
2017-11-19 11:28:21 +0545

    adding new components in components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Button from './button/Button'
+import Card from './card/Card';
+import Divider from './divider/Divider';
 import Icon from './icon/Icon';
 import Text from './text/Text';
 
-export { Button, Icon, Text };
+export { Button, Card, Divider, Icon, Text };
