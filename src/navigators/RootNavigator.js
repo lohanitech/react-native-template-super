5c30dd919361d43ee873cab4486fcb4ea054282e
Damodar Lohani <dlohani48@gmail.com>
2017-11-19 11:28:44 +0545

    root navigator now with drawer navigator

--- src/navigators/RootNavigator.js
+++ src/navigators/RootNavigator.js
@@ -1,15 +1,27 @@
-import { StackNavigator } from 'react-navigation';
-import HomeScreen  from '../screens/home';
+import { StackNavigator, DrawerNavigator } from 'react-navigation';
+import { HomeScreen, ComponentListScreen }  from '../screens';
 import globalStyles from '../styles/GlobalStyles';
 
-export default RootNavigator = StackNavigator({
-    Home: { screen: HomeScreen },
-},{
+const stackNavOptions = {
     navigationOptions: ({navigation})=>({
         headerStyle: globalStyles.headerStyle,
         headerTitleStyle: globalStyles.headerTitleStyle
     }),
+    headerType: 'screen',
     cardStyle: {
         backgroundColor: '#fff',
     }
+};
+
+export default RootNavigator = DrawerNavigator({
+    Components: { 
+        screen: StackNavigator({
+            First: {screen: ComponentListScreen}
+        },stackNavOptions)
+    },
+    Home: { 
+        screen: StackNavigator({
+            First: {screen:HomeScreen}
+        },stackNavOptions)
+    },
 });
