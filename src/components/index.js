2f4e78dc04bf5c57bd4e19124199997febe1223a
Damodar Lohani <dlohani48@gmail.com>
2017-11-24 15:28:32 +0545

    avatar component extraction in components index

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
+import Avatar from './avatar/Avatar';
 import Button from './button/Button'
 import Card from './card/Card';
 import Divider from './divider/Divider';
 import Icon from './icon/Icon';
 import Text from './text/Text';
 
-export { Button, Card, Divider, Icon, Text };
+export { Avatar, Button, Card, Divider, Icon, Text };
